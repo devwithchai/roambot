7589c339f755ca77e21a6655a35e54ff99bf663c
Chaitanya Belekar <chaitanyabelekar59@gmail.com>
2026-09-11 14:26:46 +0000

    add gazebo harmonic world and RoamBot spawning

---
+++ src/roambot_simulation/launch/empty_world.launch.py
@@ -0,0 +1,43 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    simulation_share = get_package_share_directory("roambot_simulation")
+    description_share = get_package_share_directory("roambot_description")
+    ros_gz_sim_share = get_package_share_directory("ros_gz_sim")
+
+    world_file = simulation_share + "/worlds/empty_world.sdf"
+    xacro_file = description_share + "/urdf/roambot.urdf.xacro"
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            ros_gz_sim_share + "/launch/gz_sim.launch.py"
+        ),
+        launch_arguments={
+            "gz_args": "-r -v 3 " + world_file,
+        }.items(),
+    )
+
+    spawn_roambot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-world", "empty",
+            "-name", "roambot",
+            "-string", Command(["xacro ", xacro_file]),
+            "-x", "0.0",
+            "-y", "0.0",
+            "-z", "0.08",
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription([
+        gazebo,
+        TimerAction(period=3.0, actions=[spawn_roambot]),
+    ])
--- src/roambot_simulation/setup.py
+++ src/roambot_simulation/setup.py
@@ -1,29 +1,24 @@
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'roambot_simulation'
+package_name = "roambot_simulation"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        ("share/" + package_name + "/worlds", glob("worlds/*.sdf")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ros',
-    maintainer_email='chaitanyabelekar59@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-        ],
-    },
+    maintainer="chaitanya",
+    maintainer_email="chaitanya@example.com",
+    description="Gazebo Harmonic simulation for RoamBot.",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={"console_scripts": []},
 )
